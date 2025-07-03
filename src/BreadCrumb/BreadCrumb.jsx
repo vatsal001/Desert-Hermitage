860f0e24d770d8ac4d48353808a30370ba5d7a86
Vatsal Bhatt <bhattvatsal534@gmail.com>
2025-07-03 12:10:15 +0530

    j

--- src/BreadCrumb/BreadCrumb.jsx
+++ src/BreadCrumb/BreadCrumb.jsx
@@ -4,7 +4,7 @@ const BreadCrumb = ({ title, home }) => {
   const location = useLocation();
   const pathName = location.pathname.split("/")[1];
   return (
-    <section className="bg-[url('/images/all/11.jpg')] bg-no-repeat bg-cover h-[550px] bg-center grid items-center justify-center">
+    <section className="bg-[url('/images/all/11.jpg')] bg-no-repeat bg-cover lg:h-[550px] sm:h-[290px] bg-center grid items-center justify-center">
       <div className="mt-10 text-center">
         <h1 className="text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl leading-10 lg:leading-[60px] 2xl:leading-[70px] text-white font-semibold font-Garamond uppercase">
           {title}
