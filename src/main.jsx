d095d9cadb93f66702d0662d71c964da4bb61a60
Vatsal Bhatt <bhattvatsal534@gmail.com>
2025-07-03 12:23:25 +0530

    whsapp

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router/Router.jsx";
 import { HelmetProvider } from "react-helmet-async";
+import WhatsAppChatButton from "./Shared/whatsapp.jsx";
 
 const helmetContext = {};
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider context={helmetContext}>
       <RouterProvider router={router} />
+      <WhatsAppChatButton />
     </HelmetProvider>
   </React.StrictMode>
 );
